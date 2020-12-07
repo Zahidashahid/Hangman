f40c97baccad8350d8c17128f20cae1cfb57a96d
Zahidashahid <71212009+Zahidashahid@users.noreply.github.com>
2020-12-08 02:04:55 +0500

    Initial commit

---
+++ Assets/Scripts/Hangman.cs
@@ -0,0 +1,91 @@
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.UI;
+
+
+ public class Hangman : MonoBehaviour
+ {
+    
+//     public string word = "Hello";// word to guess,
+//      char[]  letters;
+//    // private int lengthOfWord = word.Length; //
+//     // private char[] guessedLetter ; //letters player will guess
+//     // private string wordGuessed = 0;
+//     // private int retry = 6;
+//     // //private int score;  //   
+//     // private bool complete; 
+
+//     // public Button keyboardBtn;
+//     // public Text btnText;
+//     public Text wordToGuess;
+
+
+//     //char[] Letter = new char[LengthOfWord];
+//     public void StartGame()
+//     {
+//         letters = word.ToCharArray();
+//         string dashes = "";
+//         for (int p = 0; p < letters.Length; p++)
+//             dashes += "_ ";
+//         wordToGuess.text  = dashes;  
+//         Debug.Log("dashes");
+       
+
+//        // Reset();
+//     }
+
+//     // public void DisplayDashes()
+//      //{  
+//     //   ./*  for (int p = 0; p < letters.Length; p++)
+//     //         Letter[p] = '_';*/
+//     // }
+  
+
+//     // public void GetText()
+//     // {
+     
+//     //     btnText= keyboardBtn.GetComponentInChildren<Text>();
+//     //     Debug.Log(btnText.text);
+//     //     keyboardBtn.interactable = false;
+        
+//     // }
+    
+//     // public void Check()
+//     // {
+//     //     while (retry > 0  and  lettersGuessed.Length < lengthOfWord)
+//     //     {
+//     //       /*  Console.WriteLine(GuessedLetter);
+//     //         GuessedLetter.ToString().ToCharArray();
+//     //         for (int i = 1; i <= LengthOfWord; i++)
+//     //         {
+//     //             Console.Write("{0} ", GuessedLetter);
+//     //         }*/
+//     //         Console.WriteLine("Enter a letter!");
+//     //         char letter = char.Parse(Console.ReadLine());
+//     //        // for( int i = 0; i < LengthOfWord  i++) // used for a letter  2 times in word
+//     //         //{
+//     //             if (Word[i].Contains<char>(letter))
+//     //             {
+//     //                 Console.Write(letters);
+//     //             }
+//     //             else
+//     //             {
+//     //                 Console.Write("You guessed wrong!");
+                    
+//     //             }
+//     //        // }
+            
+
+//     //     }
+//    // }
+
+//         // public void Reset()
+//         // {
+//         //     score = 0;
+//         //     complete = false;
+//         //     for (int i = 0; i < hang)
+
+//         // }
+
+ }
