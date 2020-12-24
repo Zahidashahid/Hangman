64734e9f67fa1c197819ee6d8c45c80a8ff0b71b
Zahidashahid <71212009+Zahidashahid@users.noreply.github.com>
2020-12-24 12:04:01 +0500

    one word complete /without making hangman

--- Assets/Scripts/Hangman.cs
+++ Assets/Scripts/Hangman.cs
@@ -1,34 +1,51 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-[System.Serializable]
+//[System.Serializable]
 public class Hangman : MonoBehaviour
  {
     private string word = "Hello Zahi I am Java";// word to guess
     private char[] correctWordLetters;
     public Text wordToGuess;
     private char[] guessedLetters; //letters player will guess
+    int i = 0;
 
-    //    // private int lengthOfWord = word.Length; //
+   // private int lengthOfWord = word.Length; //
+    public bool ISNotComplete = true;// not guess all letters
     //     // 
     //     // private string wordGuessed = 0;
-    //     // private int retry = 6;
-    //     // //private int score;  //   
+    private int retry = 6;
+   // private int score;  //   
     //     // private bool complete; 
-
-
-
-    //     //char[] Letter = new char[LengthOfWord];
+    //  ]   //char[] Letter = new char[LengthOfWord];
     public void Start()
-    {
+    { 
         word = word.ToUpper();
         correctWordLetters = word.ToCharArray();
-        guessedLetters = new char[correctWordLetters.Length];
+        guessedLetters = new char[correctWordLetters.Length];// length of guess must be greater than correctWordLetters.Length incase of storing wrong charecter
         DisplayDashes();
     }
 
+    public void IsCompleteWord()// check all  latters are guessed
+    {
+        
+        for(int p = 0; p < correctWordLetters.Length; p++)
+        {
+            if(guessedLetters[p] == correctWordLetters[p])
+            {
+                ISNotComplete = false;
+            }
+            else
+            {
+                ISNotComplete = true;
+                break;
+                
+            }
+                
+        }
+    }      
     public void DisplayDashes()
     {
         string dashes = "";
@@ -52,88 +69,77 @@ public class Hangman : MonoBehaviour
 
     public void Check(char ch)
     {
-        bool found = false;
-        int index; 
-        for (index = 0; index < correctWordLetters.Length; index++)
+        if(retry>0 )
         {
-            if (correctWordLetters[index] == ch)
+            Debug.Log(" now trial "+retry);
+          
+            IsCompleteWord();
+            bool found = false;
+            int index; // check presence of gussed letter
+            for (index = 0; index < correctWordLetters.Length; index++)
             {
-                found = true;
-                break;
-            }
-        }
-        if (found)
-        {
-            guessedLetters[index] = correctWordLetters[index];
-            for (int p = index; p < correctWordLetters.Length; p++)
-            {
-                if (correctWordLetters[p] == correctWordLetters[index])
+                if (correctWordLetters[index] == ch)
                 {
-                    guessedLetters[p] = correctWordLetters[p];
+                    found = true;
+                    break;
                 }
             }
+            if (found)
+            {
+                i++;
+                guessedLetters[index] = correctWordLetters[index];
+                for (int p = index; p < correctWordLetters.Length; p++) // again check for multiple presence of charecter
+                {
+                    if (correctWordLetters[p] == correctWordLetters[index])
+                    {
+                        guessedLetters[p] = correctWordLetters[p];
+                    }
+                }
 
-            string currentWord = "";
-            for (int p = 0; p < guessedLetters.Length; p++)
-            {
-                if (guessedLetters[p] == ' ')
+                string currentWord = "";
+                for (int p = 0; p < guessedLetters.Length; p++)
                 {
-                    currentWord += " ";
+                    if (guessedLetters[p] == ' ')
+                    {
+                        currentWord += " ";
+                    }
+                    else
+                    {
+                        currentWord += " " + guessedLetters[p] + " ";
+                    }
                 }
-                else
-                {
-                    currentWord += " " + guessedLetters[p] + " ";
-                }
+                currentWord = currentWord.TrimStart();
+                currentWord = currentWord.TrimEnd();
+                SetWordToGuessText(currentWord);
+                Debug.Log(currentWord);
             }
-            currentWord = currentWord.TrimStart();
-            currentWord = currentWord.TrimEnd();
-            SetWordToGuessText(currentWord);
-            Debug.Log(currentWord);
+            else
+            {
+            
+                Debug.Log("Not Match!");
+                retry--;
+                Debug.Log("  after worng "+retry);
+            }
         }
         else
-            Debug.Log("Not Match!");
+        {
+             Debug.Log("  Game End now trial "+retry);
+        }
     }
+           
+        
 
     public void SetWordToGuessText(string text)
     {
         wordToGuess.text = text;
     }
 
-    //     // public void Check()
-    //     // {
-    //     //     while (retry > 0  and  lettersGuessed.Length < lengthOfWord)
-    //     //     {
-    //     //       /*  Console.WriteLine(GuessedLetter);
-    //     //         GuessedLetter.ToString().ToCharArray();
-    //     //         for (int i = 1; i <= LengthOfWord; i++)
-    //     //         {
-    //     //             Console.Write("{0} ", GuessedLetter);
-    //     //         }*/
-    //     //         Console.WriteLine("Enter a letter!");
-    //     //         char letter = char.Parse(Console.ReadLine());
-    //     //        // for( int i = 0; i < LengthOfWord  i++) // used for a letter  2 times in word
-    //     //         //{
-    //     //             if (Word[i].Contains<char>(letter))
-    //     //             {
-    //     //                 Console.Write(letters);
-    //     //             }
-    //     //             else
-    //     //             {
-    //     //                 Console.Write("You guessed wrong!");
-
-    //     //             }
-    //     //        // }
-
-
-    //     //     }
-    //    // }
-
-    //         // public void Reset()
-    //         // {
-    //         //     score = 0;
-    //         //     complete = false;
-    //         //     for (int i = 0; i < hang)
-
-    //         // }
+    // public void Reset()
+    // {
+    //    // score = 0;
+    //     complete = false;
+    //     private int retry = 6;
+    //     public bool ISNotComplete = true;
+    // }
 
 }
